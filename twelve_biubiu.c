783eedb4bde9f5eba544d0730836a010b52fdea7
XiaoHe Software <xiaohe321@outlook.com>
2019-02-20 11:31:35 +0800

    修正，更符合C语言规范；改用C82编译器。

--- twelve_biubiu.c
+++ twelve_biubiu.c
@@ -3,29 +3,33 @@
 // Author: Li.YiYi
 // Dept: PE-362, UG
 // Origin: TI-352132
+// Compiler: C82 (Version 2082-1-1)
 // 春节十二响 biu biu biu!
 
 #env "planet_engine"
 
-int init() {
+int __init() {
     set_engine_number_mask(ENGINE_ALL);
     set_funeral_level(FUNERAL_FULL);
     // 允许误差10秒以内
-    if (unix_time() < make_unix_time(2082, 1, 28, 23, 59, 60-10)) return ERR_ENGIN_ENV;
-    return engine_check_init(); // after compile and before real run
+    if (unix_time() < make_unix_time(2082, 1, 28, 23, 59, 60-10)) return ERR_ENGINE_ENV;
+    return engine_check_init(); // after compile and before the real run.
 }
+
 int main() {
     set_curve(CURVE_NATURAL); // 自然曲线耗费燃料最少
     for (int i :range(0, 12, 1)) {
         engine_start();
-        wait_engine(ENGINE_STATE_CHAGNE);
+        while(!wait_engine_ready(ENGINE_STATE_CHANGE)){};//在引擎就绪之前不可轻举妄动
         sleep(2000);
-        engin_stop();
-        wait_engine(ENGINE_STATE_CHAGNE);
+        engine_stop();
+        while(!wait_engine_ready(ENGINE_STATE_CHANGE)){};
         sleep(4000); // 这个时长在模拟器里听起来更像心跳
     }
     return 0;
 }
-int final() {
+
+int __final() {
     engine_ensure_shutdown();
+	return 0;
 }
